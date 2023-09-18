3a91474e8ba6dd2c63fff720ef4ff8733f538da5
cho4u4o <128016678+cho4u4o@users.noreply.github.com>
2023-09-18 17:10:36 +0900

    [Bronze I] Title: 2007년, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -0,0 +1,9 @@
+date = list(map(int, input().split()))
+month = date[0]
+day = date[1]
+dic = {1: 0, 2: 31, 3: 59, 4: 90, 5: 120, 6: 151, 7: 181, 8: 212, 9: 243, 10: 273, 11: 304, 12: 334}
+dayCount = dic[month] + day
+date = {1: 'MON', 2: 'TUE', 3: 'WED', 4: 'THU', 5: 'FRI', 6: 'SAT', 0: 'SUN'}
+print(date[dayCount % 7])
+
+
