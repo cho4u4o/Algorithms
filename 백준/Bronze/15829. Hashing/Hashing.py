22f114f1e90de57659174fe9ab88330558d9b93b
cho4u4o <128016678+cho4u4o@users.noreply.github.com>
2023-09-18 17:26:04 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,11 @@
+def func_pow(x):
+    return(pow(31, x))
+def func_hashing(x):
+    return(x - 96)
+N = int(input())
+num_pow = []
+inputs = list(map(ord, input()))
+nums = list(map(func_hashing, inputs))
+for i in range(0, N):
+    num_pow.append(nums[i]*func_pow(i))
+print(sum(num_pow))
